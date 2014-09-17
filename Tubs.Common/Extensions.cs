db1d0c197194a33ab0c78e175dcdd7a723b88eb3
Melvin <melvin@melrose27.com>
2014-09-17 16:21:16 -0700

    Added new Tubs.Common project and linq foreach extension method.

---
+++ Tubs.Common/Extensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tubs.Common
+{
+    public static class Extensions
+    {
+        public static void ForEach<T>(this IEnumerable<T> enumeration, Action<T> action)
+        {
+            foreach (T item in enumeration)
+            {
+                action(item);
+            }
+        }
+    }
+}
